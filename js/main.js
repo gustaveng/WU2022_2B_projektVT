1759ee10eb5cdec6ee6f28cb76f1f9b92b663504
Gustav Engstrom <83348326+gustaveng@users.noreply.github.com>
2022-03-24 14:19:29 +0100

    2 pages added

--- js/main.js
+++ js/main.js
@@ -4,3 +4,9 @@ const nav = document.querySelector(".nav")
 burger.addEventListener("click", () => {
     nav.classList.toggle("toggle")
 })
+
+function toggleMenu(){
+    burger.classList.toggle('burger-toggle')
+}
+
+burger.addEventListener('click', toggleMenu)
